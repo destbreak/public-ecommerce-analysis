d296de7b426d62edd72c73a33eff70645bcd03d5
DestBreak <rey.hadiansyah7th@gmail.com>
2023-10-06 21:27:55 +0700

    Update palette's plot

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -24,10 +24,11 @@ with tab1:
     with st.container(): 
         # Jenis Produk
         product_category = products.product_category_name_english.value_counts()
+        colors = ['#329932','#999999','#999999','#999999','#999999','#999999','#999999']
 
         st.subheader('Produk yang dijual di E-Commerce')
         fig_product_category = plt.figure(figsize=(12.8, 9.6))
-        sns.barplot(x=product_category.index, y=product_category)
+        sns.barplot(x=product_category.index, y=product_category, palette=colors)
         plt.xlabel('Jenis Produk')
         plt.ylabel('Jumlah')
 
@@ -45,10 +46,11 @@ with tab2:
     with st.container(): 
         # Metode Pembayaran
         payment_type = payments.payment_type.value_counts()
+        colors = ['#329932','#999999','#999999','#999999']
 
         st.subheader('Metode Pembayaran yang digunakan ketika transaksi di e-commerce')
         fig_payment_type = plt.figure(figsize=(12.8, 9.6))
-        sns.barplot(x=payment_type.index[:4], y=payment_type[:4])
+        sns.barplot(x=payment_type.index[:4], y=payment_type[:4], palette=colors)
         plt.xlabel('Metode Pembayaran')
         plt.ylabel('Jumlah')
 
@@ -64,10 +66,11 @@ with tab2:
 
         # Skema Cicilan
         payment_installments = payments.payment_installments.value_counts().sort_index()
+        colors = ['#329932','#999999','#999999','#999999','#999999','#999999','#999999','#999999','#999999','#999999','#999999','#999999']
 
         st.subheader('Skema cicilan yang digunakan dalam transaksi di e-commerce\n(kurun waktu 1-12 bulan)')
         fig_payment_installments = plt.figure(figsize=(12.8, 9.6))
-        sns.barplot(x=payment_installments.index[1:13], y=payment_installments[1:13])
+        sns.barplot(x=payment_installments.index[1:13], y=payment_installments[1:13], palette=colors)
         plt.xlabel('Skema Cicilan (x bulan)')
         plt.ylabel('Jumlah')
 
@@ -85,10 +88,11 @@ with tab3:
     with st.container(): 
         # 10 Kota Pembeli
         city_customers = geo_customers.customer_city.value_counts()
+        colors = ['#329932','#999999','#999999','#999999','#999999','#999999','#999999','#999999','#999999','#999999']
 
         st.subheader('10 Kota Pembeli banyak berasal')
         fig_city_customers = plt.figure(figsize=(12.8, 9.6))
-        sns.barplot(x=city_customers, y=city_customers.index)
+        sns.barplot(x=city_customers, y=city_customers.index, palette=colors)
         plt.xlabel('Jumlah')
         plt.ylabel('Kota')
         
@@ -137,10 +141,11 @@ with tab4:
     with st.container(): 
         # 10 Kota Penjual
         city_sellers = geo_sellers.seller_city.value_counts()
+        colors = ['#329932','#999999','#999999','#999999','#999999','#999999','#999999','#999999','#999999','#999999']
 
         st.subheader('10 Kota Penjual banyak berasal')
         fig_city_sellers = plt.figure(figsize=(12.8, 9.6))
-        sns.barplot(x=city_sellers, y=city_sellers.index)
+        sns.barplot(x=city_sellers, y=city_sellers.index, palette=colors)
         plt.xlabel('Jumlah')
         plt.ylabel('Kota')
 
